f217dd5f5664a5cda510c14a97fdd2bae8c82885
Sown21 <marco.lezzi@focus.lu>
2023-11-28 17:03:49 +0100

    update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,20 @@ exit_with_error() {
 	exit 1
 }
 
+install_docker() {
+        echo -e "\nInstallation de Docker..."
+        sudo apt-get update
+        sudo apt-get install -y docker.io
+        sudo usermod -aG docker root
+}
+
+if ! command -v docker &> /dev/null; then
+        install_docker || exit_with_error "Echec de l'installation de Docker"
+        echo -e "\nDocker installé avec succès"
+else
+        echo -e "\nDocker est déjà installé"
+fi
+
 echo -e "Création du dossier ubuntu_docker_save..."
 sudo mkdir -p /etc/ubuntu_docker_save || exit_with_error "Impossible de créer le dossier"
 
